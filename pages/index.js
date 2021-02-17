898742800d4bf2874ed13dedc07035da31d76106
Fernando Pena <nando.pena@gmail.com>
2021-02-16 23:05:42 -0300

    Estrutura

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 // import styled from 'styled-components';
 import Header from '../src/components/commons/Header';
+import Info from '../src/components/commons/Info';
+import Main from '../src/components/commons/Main';
+import Footer from '../src/components/commons/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Info />
+      <Main />
+      <Footer />
     </>
   );
 }
