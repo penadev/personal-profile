79e07bbec0b9364823840e13f283c8bee750e6d9
Fernando Pena <nando.pena@gmail.com>
2021-02-19 20:13:29 -0300

    correcao caminho meta tab

--- pages/_app.js
+++ pages/_app.js
@@ -18,13 +18,13 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://pena.rocks/" />
         <meta property="og:title" content="Fernando Pena - Arquiteto de Soluções" />
         <meta property="og:description" content="Arquiteto de Soluções e Desenvolvedor FullStack Senior - Perfil Profissional" />
-        <meta property="og:image" content="../images/meta_tag.png" />
+        <meta property="og:image" content="/images/meta_tag.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://pena.rocks/" />
         <meta property="twitter:title" content="Fernando Pena - Arquiteto de Soluções" />
         <meta property="twitter:description" content="Arquiteto de Soluções e Desenvolvedor FullStack Senior - Perfil Profissional" />
-        <meta property="twitter:image" content="../images/meta_tag.png" />
+        <meta property="twitter:image" content="/images/meta_tag.png" />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Lato:wght@400;700&family=Libre+Barcode+39+Text&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
