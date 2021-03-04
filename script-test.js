14f17ed3232c92021f4e7e3ebd40207eb31b9818
Fernando Pena <nando.pena@gmail.com>
2021-03-03 22:20:41 -0300

    ci(lint): enforce husky with commitlint

--- script-test.js
+++ script-test.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-console */
 const shell = require('shelljs');
 
 const resultado = shell.exec('git diff --name-only branch-scripts-testes..main', { silent: true });
